4c5d420f2e139d69fcb36ed2a7241eb42518e77b
marcelaportela <portela.marcela@hotmail.com>
2018-07-21 18:58:57 -0300

    Primeiro commit

---
+++ js/questions.js
@@ -0,0 +1,26 @@
+var questions = [{
+  question: "Qual o papel da Universidade? ",
+  choices: ['Produzir novos conhecimentos e aplicá-los à realidade social', 'Somente serve pra conseguir um diploma', 'conseguir bolsa', 'para produzir produtos'],
+  correctAnswer: 0,
+  answerLocation: '"Produzir novos conhecimentos e aplicá-los à realidade social"'
+}, {
+  question: "Qual das seguintes áreas a Epitrack não atua?",
+  choices: ['Plataformas de detecção digital de doenças','Vizgram','Clinio','Git'],
+  correctAnswer: 3,
+  answerLocation: '"Git"'
+}, {
+  question: "O que é rastreamento?",
+  choices: ['A capacidade de rastrear a história, a aplicação ou a localização daquilo que estásendo considerado.', 'Nenhuma', 'É a habilidade de rastrear a identidade de pessoas', 'É a capacidade de rastrear e a aplicação , mas não a localização'],
+  correctAnswer: 0,
+  answerLocation: '"A capacidade de rastrear a história, a aplicação ou a localização daquilo que estásendo considerado."'
+}, {
+  question: "Qual a alternativa incorreta em relação à computação evolucionária?",
+  choices: ['AEs enquadram-se na categoria dos algoritmos de “gerar e testar”', 'Os operadores de variação (recombinação e mutação) criam a diversidade necessária, mas não facilitam novas descobertas', 'Seleção reduz a diversidade e atua como uma força, impulsionando a qualidade', 'Eles são algoritmos estocásticos, baseados em população'],
+  correctAnswer: 1,
+  answerLocation: '"Os operadores de variação (recombinação e mutação) criam a diversidade necessária, mas não facilitam novas descobertas"'
+}, {
+  question: "O que é realidade virtual?",
+  choices: ['Informações virtuais são sobrepostas à realidade em uma tela comum,', 'É uma realidade imaginária', 'É um ecossistema de tecnologias e linguagens que possibilitam que o usuário se sinta imerso em uma realidade totalmente simulada digitalmente.', 'É a mesma coisa que realidade aumentada'],
+  correctAnswer: 2,
+  answerLocation: '"Os operadores de variação (recombinação e mutação) criam a diversidade necessária, mas não facilitam novas descobertas"'
+}];
